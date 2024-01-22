aa42c1530503fabe9e5474acf1377ea1c2fdbb14
Alex <9138@htl.rennweg.at>
2024-01-22 15:27:42 +0100

    etwas pydoc hinzugefügt

--- sew5p/UE02_Labyrinth/Labyrinth.py
+++ sew5p/UE02_Labyrinth/Labyrinth.py
@@ -22,6 +22,15 @@ def print_labyrinth(lab: [[str]]) -> None:
 
 
 def suchen(zeile: int, spalte: int, lab: [[str]], should_print: bool, delay: int):
+    """
+    Löst Labyrinth
+    :param zeile: Startzeile
+    :param spalte: Startspalte
+    :param lab: zu lösendes Labyrinth
+    :param should_print: soll die Lösung ausgegeben werden?
+    :param delay: delay zwischen ausgabe der Lösungen
+    :return:
+    """
     if lab[zeile][spalte] == 'A':
         if should_print:
             print_labyrinth(lab)
@@ -31,33 +40,37 @@ def suchen(zeile: int, spalte: int, lab: [[str]], should_print: bool, delay: int
         return False
     lab[zeile][spalte] = 'x'
     n1 = suchen(zeile - 1, spalte, lab, should_print, delay)
-    n1 += suchen(zeile + 1, spalte, lab, should_print, delay)
-    n1 += suchen(zeile, spalte - 1, lab, should_print, delay)
-    n1 += suchen(zeile, spalte + 1, lab, should_print, delay)
+    n2 = suchen(zeile + 1, spalte, lab, should_print, delay)
+    n3 = suchen(zeile, spalte - 1, lab, should_print, delay)
+    n4 = suchen(zeile, spalte + 1, lab, should_print, delay)
     lab[zeile][spalte] = ' '
-    return n1
+    return n1 or n2 or n3 or n4
 
 
-def sucheAlle(zeile: int, spalte: int, lab: [[str]]):
+def sucheAlle(zeile: int, spalte: int, lab: [[str]], should_print: bool, delay: int):
     """
     sucht alle möglichen Wege zu einem Ausgang
-    :param zeile:
-    :param spalte:
-    :param lab:
-    :return:
+    :param zeile: Startzeile
+    :param spalte: Startspalte
+    :param lab: zu lösendes Labyrinth
+    :param should_print: sollen die Lösungen ausgegeben werden?
+    :param delay: delay zwischen den Lösungen
+    :return: Anzahl der möglichen Pfade
     """
     if lab[zeile][spalte] == 'A':
-        print_labyrinth(lab)
+        if should_print:
+            print_labyrinth(lab)
+            time.sleep(delay / 1000)
         return 1
     if lab[zeile][spalte] == '#' or lab[zeile][spalte] == 'x':
         return 0
     lab[zeile][spalte] = 'x'
-    n1 = sucheAlle(zeile - 1, spalte, lab)
-    n2 = sucheAlle(zeile + 1, spalte, lab)
-    n3 = sucheAlle(zeile, spalte - 1, lab)
-    n4 = sucheAlle(zeile, spalte + 1, lab)
+    n1 = sucheAlle(zeile - 1, spalte, lab, should_print, delay)
+    n1 += sucheAlle(zeile + 1, spalte, lab, should_print, delay)
+    n1 += sucheAlle(zeile, spalte - 1, lab, should_print, delay)
+    n1 += sucheAlle(zeile, spalte + 1, lab, should_print, delay)
     lab[zeile][spalte] = ' '
-    return n1 or n2 or n3 or n4
+    return n1
 
 
 parser = argparse.ArgumentParser()
@@ -71,8 +84,9 @@ args = parser.parse_args()
 
 lab = read_from_file(args.filename)
 start_time = int(time.time()*1000)
-suchen(int(args.xstart), int(args.ystart), lab, args.print, int(args.delay))
+res = sucheAlle(int(args.xstart), int(args.ystart), lab, args.print, int(args.delay))
 end_time = int(time.time()*1000)
+print(f'possible ways: {res}')
 if args.time:
     print(f'Time Elapse: {(end_time-start_time)/1000} ms')
 
