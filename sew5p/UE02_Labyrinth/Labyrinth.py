149f79e29988f81ec877cace54f054c39a748b06
Alex <9138@htl.rennweg.at>
2024-01-22 13:08:22 +0100

    args added

--- sew5p/UE02_Labyrinth/Labyrinth.py
+++ sew5p/UE02_Labyrinth/Labyrinth.py
@@ -1,3 +1,5 @@
+import argparse
+
 
 def read_from_file(filepath: str):
     """
@@ -26,5 +28,28 @@ def suchen(zeile: int, spalte: int, lab: [[str]]):
     lab[zeile][spalte] = ' '
     return n1 or n2 or n3 or n4
 
+def sucheAlle(zeile: int, spalte: int, lab: [[str]]):
+    if lab[zeile][spalte] == 'A':
+        print_labyrinth(lab)
+        return 1
+    if lab[zeile][spalte] == '#' or lab[zeile][spalte] == 'x':
+        return 0
+    lab[zeile][spalte] = 'x'
+    n1 = sucheAlle(zeile - 1, spalte, lab)
+    n1 += sucheAlle(zeile + 1, spalte, lab)
+    n1 += sucheAlle(zeile, spalte - 1, lab)
+    n1 += sucheAlle(zeile, spalte + 1, lab)
+    lab[zeile][spalte] = ' '
+    return n1
+
 print_labyrinth(read_from_file("l1.txt"))
-suchen(1, 1, read_from_file('./l1.txt'))
+print(sucheAlle(5, 5, read_from_file('./l1.txt')))
+
+parser = argparse.ArgumentParser()
+parser.add_argument("filename", help="file containing the labyrinth to solve")
+parser.add_argument("-h", "--help", help="show this help message and exit")
+parser.add_argument("-x", "--xstart", help="x-coordinate to start")
+parser.add_argument("-y", "--ystart", help="y-coordinate to start")
+parser.add_argument("-p", "--print", help="print output of every solution",action="store_true")
+parser.add_argument("-d", "--delay", help="delay after printing a solution")
+args = parser.parse_args()
